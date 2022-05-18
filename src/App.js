4d380d7f17b8e115091c99bcb4fc01ec8e9298dd
Manoel Castro <manoelcl@gmail.com>
2022-05-19 01:14:00 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useState } from "react";
 function App() {
+  const [birras, setBirras] = useState();
+  const [text, setText] = useState("");
+  const cargarBirras = () => {
+    fetch("https://api.punkapi.com/v2/beers/")
+      .then((response) => response.json())
+      .then((response) => setBirras(response));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={cargarBirras}>Cargar cervezas</button>
+      <input
+        type="text"
+        onChange={(e) => setText(e.target.value.toUpperCase())}
+      />
+      <div className="cervezas">
+        {birras
+          ? birras
+              .filter(
+                (birra) =>
+                  birra.description.toUpperCase().search(text) !== -1 ||
+                  birra.name.toUpperCase().search(text) !== -1
+              )
+              .map((birra) => (
+                <article key={birra.id} className="card">
+                  <h2>{birra.name}</h2>
+                  <p>{birra.description}</p>
+                  <img src={birra.image_url} alt={birra.name} />
+                </article>
+              ))
+          : null}
+      </div>
     </div>
   );
 }
